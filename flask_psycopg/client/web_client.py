379d1eda44d4323b248048727f823a8ba04ed496
filka657 <72990504+filka657@users.noreply.github.com>
2022-12-18 12:36:10 +0300

    Update web_client.py

--- flask_psycopg/client/web_client.py
+++ flask_psycopg/client/web_client.py
@@ -10,8 +10,8 @@ print(r.status_code, r.headers, type(r.text), r.json())
 # pprint(r.json())
 
 # r = requests.post('http://127.0.0.1:5000/users', json={
-#     "user_name": "John",
-#     "user_surname": "Wayne"
+#     "user_name": "Tonya",
+#     "user_surname": "Filatova"
 #     })
 # r = requests.post('http://127.0.0.1:5000/users', json={
 #     "user_name": "Red",
@@ -21,13 +21,11 @@ print(r.status_code, r.headers, type(r.text), r.json())
 
 r = requests.put('http://127.0.0.1:5000/users', json={
     "user_id": 10,
-    "user_name": "Red",
-    "user_surname": "Green"
+    "user_name": "Yellow",
+    "user_surname": "Red"
     })
 # pprint(r.json())
 # r = requests.delete('http://127.0.0.1:5000/users', json={
 #     "user_id": 5,
 #     })
 # pprint(r.text)
-
-
